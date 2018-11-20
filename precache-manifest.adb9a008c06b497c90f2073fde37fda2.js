37e524a8bd3ff72068192b9bc8e6e27b5f2839bf
morthos <pathshan61@gmail.com>
2018-11-20 13:37:56 -0500

    Updates

---
+++ precache-manifest.adb9a008c06b497c90f2073fde37fda2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec6302c19665ba2e46f",
+    "url": "/Pk013/Todo.git/static/js/runtime~main.aec6302c.js"
+  },
+  {
+    "revision": "181fab642985f49772c5",
+    "url": "/Pk013/Todo.git/static/js/main.181fab64.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/Pk013/Todo.git/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "181fab642985f49772c5",
+    "url": "/Pk013/Todo.git/static/css/main.eb9af8ee.chunk.css"
+  },
+  {
+    "revision": "2d638171a9ca7724f1c9acaa13958a57",
+    "url": "/Pk013/Todo.git/index.html"
+  }
+];
